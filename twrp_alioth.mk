6637067f8023e462cdcdf3a4e1613ebbb6789121
rtyutechstudio <rtyutechstudio@icloud.com>
2026-04-04 18:23:10 +0800

    dfbsd

--- twrp_alioth.mk
+++ twrp_alioth.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 
 # Inherit from lion device
-$(call inherit-product, device/infinix/alioth/device.mk)
+$(call inherit-product, device/google/alioth/device.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
