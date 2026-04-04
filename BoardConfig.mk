25e8b71b9fe9d76b11df6228fb704460052baaf7
rtyutechstudio <rtyutechstudio@icloud.com>
2026-04-04 18:28:22 +0800

    boardconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/google/alitoh
+DEVICE_PATH := device/google/alioth
 
 # For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
